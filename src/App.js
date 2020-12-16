b05bf2041954b83eabf841b1daeabf3bac9d543e
Andrew McManus <andrewmcmanusmusic@gmail.com>
2020-12-16 10:42:36 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Show from './pages/Show';
+import New from './pages/New';
+import Edit from './pages/Edit';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1>Bounties</h1>
+        <Home />
+        <Show />
+        <New />
+        <Edit />
+      </div>
+    );
+  }
 }
 
 export default App;
